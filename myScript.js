f32e0edd75401fbd985f7de338c9357a8652a5dc
Markus Bader <baderm14@unet.univie.ac.at>
2017-06-25 19:11:20 +0200

    changing color by hovering over it works now. TODO: With more colors and/or grey/levels

--- myScript.js
+++ myScript.js
@@ -1,16 +1,39 @@
 $(document).ready(function() {
-	draw(6);
-
+	draw(4);
+	$( "td" ).mouseenter(function(){
+		console.log("color: ", $(this).attr("bgcolor"));
+		
+		if($(this).attr("bgcolor") == 'black'){
+			$(this).attr('bgcolor','white');
+		}else{
+			$(this).attr('bgcolor','black');
+		}
+	});	
 });
+
+
+
+
+
+function askForSize(){
+	var n = prompt("How many rows/cols?", 4);
+	var elem = document.getElementById('tab');
+  	elem.parentNode.removeChild(elem);
+  	draw(n);
+}
+
+
+
+
 
 function draw(n){
 	var m = n;
-	var html = "<table>"; 
+	var html = "<table id='tab'>"; 
 	for(var i = 0; i<n; i++){
 		html += "<tr>";
 		for(var j = 0; j<m;j++){
 			if((i%2==0 && j%2==0) || (i%2==1 && j%2==1)) {
-				html+= "<td bgcolor = 'black'></td>";
+				html+= "<td bgcolor = 'white'></td>";
 			}
 			if((i%2==1 && j%2==0) || (i%2==0 && j%2==1)){
 				html+= "<td bgcolor = 'white'></td>";
